fixing RMSE calculation

eonr/eonr.py
@@ -119,11 +119,13 @@ class EONR(object):
         self.df_results = pd.DataFrame(columns=['price_grain', 'cost_n_fert',
                                                 'cost_n_social', 'price_ratio',
                                                 'location', 'year', 'time_n',
-                                                'eonr', 'ci_level',
+                                                'base_zero', 'eonr',
+                                                'ci_level',
                                                 'ci_wald_l', 'ci_wald_u',
                                                 'ci_pl_l', 'ci_pl_u',
                                                 'ci_boot_l', 'ci_boot_u',
                                                 'mrtn', 'grtn_r2_adj',
+                                                'grtn_rmse',
                                                 'grtn_max_y', 'grtn_crit_x',
                                                 'grtn_y_int', 'scn_lin_r2',
                                                 'scn_lin_rmse', 'scn_exp_r2',
@@ -396,7 +398,8 @@ class EONR(object):
             self.coefs_social['exp_r2'] = 0
             self.coefs_social['exp_rmse'] = None
         else:
-            exp_r2, _, _, _, exp_rmse = self._get_rsq(self._f_exp, x, y, popt)
+            exp_r2, _, _, _, exp_rmse = self._get_rsq(self._f_exp, x, y,
+                                                         popt)
             a, b, c = popt
             if self.print_out is True:
                 print('y = {0:.5} * exp({1:.5}x) + {2:.5} '.format(a, b, c))
@@ -593,9 +596,10 @@ class EONR(object):
         <func> with <popt> parameters
         '''
         res = y - func(x, *popt)
-        ss_res = np.sum(res**2)
-        rmse = (ss_res)**0.5
+        ss_res_mean = np.mean(res**2)
+        rmse = (ss_res_mean)**0.5
         y_mean = np.mean(y)
+        ss_res = np.sum(res**2)
         ss_tot = np.sum((y-y_mean)**2)
         r2 = 1-ss_res/ss_tot
         p = len(popt)
@@ -621,22 +625,28 @@ class EONR(object):
             self.linspace_cost_n_social = (x1, y_social_n)
         else:
             rtn = y_grtn - y_fert_n
-        y_max = (self.coefs_grtn['coef_a'].n +
-                 (self.coefs_grtn['crit_x'] *
-                  self.coefs_grtn['coef_b'].n) +
-                 (self.coefs_grtn['crit_x'] *
-                  self.coefs_grtn['crit_x'] *
-                  self.coefs_grtn['coef_c'].n))
-        # Find index where all x = max
-        y_temp = (self.coefs_grtn['coef_a'].n +
-                  (x1*self.coefs_grtn['coef_b'].n) +
-                  (x1*x1*self.coefs_grtn['coef_c'].n))
-        y_max_idx = np.argmax(y_temp)
-        y2a = (self.coefs_grtn['coef_a'].n +
-               (x1a[:y_max_idx]*self.coefs_grtn['coef_b'].n) +
-               (x1a[:y_max_idx]*x1a[:y_max_idx]*self.coefs_grtn['coef_c'].n))
-        y2b = np.linspace(y_max, y_max, num=n_steps-y_max_idx)
-        y_grtn = np.concatenate((y2a, y2b))
+#        y_max = (self.coefs_grtn['coef_a'].n +
+#                 (self.coefs_grtn['crit_x'] *
+#                  self.coefs_grtn['coef_b'].n) +
+#                 (self.coefs_grtn['crit_x'] *
+#                  self.coefs_grtn['crit_x'] *
+#                  self.coefs_grtn['coef_c'].n))
+#        # Find index where all x = max
+#        y_temp = (self.coefs_grtn['coef_a'].n +
+#                  (x1*self.coefs_grtn['coef_b'].n) +
+#                  (x1*x1*self.coefs_grtn['coef_c'].n))
+#        y_max_idx = np.argmax(y_temp)
+#        y2a = (self.coefs_grtn['coef_a'].n +
+#               (x1a[:y_max_idx]*self.coefs_grtn['coef_b'].n) +
+#               (x1a[:y_max_idx]*x1a[:y_max_idx]*self.coefs_grtn['coef_c'].n))
+#        if self.eonr <= self.df_data[self.col_n_app].max():
+#            y2b = np.linspace(y_max, y_max, num=n_steps-y_max_idx)
+#        else:  # EONR is past the point of available data, plot last val again
+#            last_pt = (self.coefs_grtn['coef_a'].n +
+#                       (x1a[-1]*self.coefs_grtn['coef_b'].n) +
+#                       (x1a[-1]*x1a[-1]*self.coefs_grtn['coef_c'].n))
+#            y2b = np.linspace(last_pt, last_pt, num=n_steps-y_max_idx)
+#        y_grtn = np.concatenate((y2a, y2b))
 
         while len(y_grtn) != n_steps:
             if len(y_grtn) < n_steps:
@@ -842,8 +852,8 @@ class EONR(object):
 
         crit_x = -beta1.n/(2*beta2.n)
         max_y = self._f_quad_plateau(crit_x, beta0.n, beta1.n, beta2.n)
-        r2, r2_adj, ss_res, ss_tot, rmse = self._get_rsq(self._f_quad_plateau,
-                                                         x, y, popt)
+        r2, r2_adj, ss_res, ss_tot, rmse = self._get_rsq(
+                self._f_quad_plateau, x, y, popt)
         aic = self._calc_aic(x, y, dist='gamma')
 
         if rerun is False:
@@ -934,7 +944,13 @@ class EONR(object):
         y2a = (self.coefs_grtn['coef_a'].n +
                (x1a[:y_max_idx]*self.coefs_grtn['coef_b'].n) +
                (x1a[:y_max_idx]*x1a[:y_max_idx]*self.coefs_grtn['coef_c'].n))
-        y2b = np.linspace(y_max, y_max, num=n_steps-y_max_idx)
+        if self.eonr <= self.df_data[self.col_n_app].max():
+            y2b = np.linspace(y_max, y_max, num=n_steps-y_max_idx)
+        else:  # EONR is past the point of available data, plot last val again
+            last_pt = (self.coefs_grtn['coef_a'].n +
+                       (x1a[-1]*self.coefs_grtn['coef_b'].n) +
+                       (x1a[-1]*x1a[-1]*self.coefs_grtn['coef_c'].n))
+            y2b = np.linspace(last_pt, last_pt, num=n_steps-y_max_idx)
         y_grtn = np.concatenate((y2a, y2b))
 
         # if necessary, modify y_grtn so it has correct number of values
@@ -1389,6 +1405,7 @@ class EONR(object):
                                     self.price_grain,
                                     self.cost_n_fert,
                                     self.cost_n_social,
+                                    self.price_ratio,
                                     np.nan, t_stat, level,
                                     wald_l, wald_u, np.nan, np.nan]],
                                   columns=df_ci.columns)
@@ -1467,7 +1484,7 @@ class EONR(object):
         return df_ci
 
     #  Following are plotting functions
-    def _add_labels(self, g):
+    def _add_labels(self, g, x_max):
         '''
         Adds EONR and economic labels to the plot
         '''
@@ -1477,10 +1494,11 @@ class EONR(object):
 
         label_eonr = 'EONR: {0:.0f} {1}\nMRTN: ${2:.2f}'.format(
                 self.eonr, self.unit_nrate, self.mrtn)
-        g.ax.plot([self.eonr], [self.mrtn], marker='.', markersize=15,
-                  color=self.palette[2], markeredgecolor='white',
-                  label=label_eonr)
-        if self.eonr > 0:
+        if self.eonr <= x_max:
+            g.ax.plot([self.eonr], [self.mrtn], marker='.', markersize=15,
+                      color=self.palette[2], markeredgecolor='white',
+                      label=label_eonr)
+        if self.eonr > 0 and self.eonr < self.df_data[self.col_n_app].max():
             g.ax.annotate(
                 label_eonr,
                 xy=(self.eonr, self.mrtn), xytext=(-80, -30),
@@ -1578,15 +1596,31 @@ class EONR(object):
         if ci_level is None:
             ci_level = self.ci_level
         label_ci = ('Confidence ({0:.2f})'.format(ci_level))
-        g.ax.axvline(ci_l, linestyle='-', linewidth=0.5, color='#7b7b7b',
-                     label=label_ci)
-        g.ax.axvline(ci_u, linestyle='-', linewidth=0.5, color='#7b7b7b',
-                     label=None)
-        g.ax.axvline(self.eonr,
-                     linestyle='--',
-                     linewidth=1.5,
-                     color='k',
-                     label='EONR')
+
+        if self.eonr <= self.df_data[self.col_n_app].max():
+            alpha_axvline = 1
+            g.ax.axvline(ci_l, linestyle='-', linewidth=0.5, color='#7b7b7b',
+                         label=label_ci, alpha=alpha_axvline)
+            g.ax.axvline(ci_u, linestyle='-', linewidth=0.5, color='#7b7b7b',
+                         label=None, alpha=alpha_axvline)
+            g.ax.axvline(self.eonr,
+                         linestyle='--',
+                         linewidth=1.5,
+                         color='k',
+                         label='EONR',
+                         alpha=alpha_axvline)
+        else:
+            alpha_axvline = 0
+            g.ax.axvline(0, linestyle='-', linewidth=0.5, color='#7b7b7b',
+                         label=label_ci, alpha=alpha_axvline)
+            g.ax.axvline(0, linestyle='-', linewidth=0.5, color='#7b7b7b',
+                         label=None, alpha=alpha_axvline)
+            g.ax.axvline(0,
+                         linestyle='--',
+                         linewidth=1.5,
+                         color='k',
+                         label='EONR',
+                         alpha=alpha_axvline)
         return g
 
     def _cmap_to_palette(self, cmap):
@@ -1616,7 +1650,7 @@ class EONR(object):
             palette = self._cmap_to_palette(colors)
         return palette
 
-    def _draw_n_cost(self, g, palette):
+    def _draw_lines(self, g, palette):
         '''
         Draws N cost on plot
         '''
@@ -1838,14 +1872,15 @@ class EONR(object):
         self._print_results()
         results = [[self.price_grain, self.cost_n_fert, self.cost_n_social,
                     self.price_ratio, self.location, self.year, self.time_n,
-                    self.eonr, self.ci_level,
-                    self.df_ci_temp['wald_l'].item(),
+                    self.coefs_grtn_primary['coef_a'].n, self.eonr,
+                    self.ci_level, self.df_ci_temp['wald_l'].item(),
                     self.df_ci_temp['wald_u'].item(),
                     self.df_ci_temp['pl_l'].item(),
                     self.df_ci_temp['pl_u'].item(),
                     self.df_ci_temp['boot_l'].item(),
                     self.df_ci_temp['boot_u'].item(),
                     self.mrtn, self.coefs_grtn['r2_adj'],
+                    self.coefs_grtn['rmse'],
                     self.coefs_grtn['max_y'],
                     self.coefs_grtn['crit_x'],
                     self.results_temp['grtn_y_int'],
@@ -1867,7 +1902,8 @@ class EONR(object):
         self._print_results()
 
     def plot_eonr(self, x_min=None, x_max=None, y_min=None, y_max=None,
-                  style='ggplot', ci_type='profile-likelihood', ci_level=None):
+                  style='ggplot', ci_type='profile-likelihood', ci_level=None,
+                  idx=None):
         '''
         Plot EONR, MRTN, GRTN, and N cost
         <style> can be any of the options supported by matplotlib:
@@ -1898,13 +1934,13 @@ class EONR(object):
                       'not be plotted..'.format(err))
         self._modify_axes_labels(fontsize=14)
         g = self._add_ci(g, ci_type=ci_type, ci_level=ci_level)
-        g = self._draw_n_cost(g, self.palette)
+        g = self._draw_lines(g, self.palette)
         g = self._modify_minmax_axes(g, x_min, x_max, y_min, y_max)
         g = self._modify_axes_pos(g)
         g = self._place_legend(g)
         g = self._modify_plot_params(g, plotsize_x=7, plotsize_y=4,
                                      labelsize=11)
-        g = self._add_labels(g)
+        g = self._add_labels(g, x_max)
         g = self._add_title(g)
         plt.tight_layout()
 

script/nue_strips_troubleshooting.py
@@ -47,7 +47,7 @@ df = df_willmar_plots.dropna(subset=['dem_mean', 'j_yld_grai'])
 df_0 = df.loc[df['nrate'] == 0]
 df_280 = df.loc[df['nrate'] == 280]
 
-def plot_scatter(df, x_plt='dem_mean', y_plt='j_yld_grai'):
+def plot_scatter(df, x_plt='dem_mean', y_plt='j_yld_grai', col_nrate='nrate'):
     ax = sns.scatterplot(x=x_plt, y=y_plt, hue='rep', data=df)
 
     x = df[x_plt]
@@ -65,11 +65,11 @@ def plot_scatter(df, x_plt='dem_mean', y_plt='j_yld_grai'):
     plt.tight_layout()
 
     print(str_eq)
-    g = sns.FacetGrid(df, col='nrate', hue='rep', height=3, aspect=0.8)
+    g = sns.FacetGrid(df, col=col_nrate, hue='rep', height=3, aspect=0.8)
     g = g.map(plt.scatter, x_plt, y_plt)
     axes = g.axes[0]
     for idx, ax in enumerate(axes):
-        df_temp = df.loc[df['nrate'] == g.col_names[idx]]
+        df_temp = df.loc[df[col_nrate] == g.col_names[idx]]
 #        sns.scatterplot(x_plt, y_plt, data=df_temp, ax=ax)
 
         x = df_temp[x_plt]
@@ -101,3 +101,96 @@ df_willmar_plots.loc[df_willmar_plots['rep'].isin(high), 'low_high'] = 1
 
 df_willmar_low = df_willmar_plots[df_willmar_plots['low_high'] == 0]
 df_willmar_high = df_willmar_plots[df_willmar_plots['low_high'] == 1]
+
+
+# In[Print unique N rates]
+sorted(df_nue12g_pre['rate_n_applied_kgha'].unique())
+sorted(df_nue12s_pre['rate_n_applied_kgha'].unique())
+sorted(df_nue13j_pre['rate_n_applied_kgha'].unique())
+sorted(df_nue13wil_pre['rate_n_applied_kgha'].unique())
+sorted(df_nue14nr_pre['rate_n_applied_kgha'].unique())
+sorted(df_nue14sc_pre['rate_n_applied_kgha'].unique())
+
+sorted(df_sns15s['rate_n_applied_kgha'].unique())
+sorted(df_sns15w['rate_n_applied_kgha'].unique())
+sorted(df_sns16w['rate_n_applied_kgha'].unique())
+sorted(df_sns17w['rate_n_applied_kgha'].unique())
+
+
+nue_sites = [df_nue12g_pre, df_nue12s_pre, df_nue13j_pre, df_nue13wil_pre, df_nue14nr_pre, df_nue14sc_pre]
+sns_sites = [df_sns15s, df_sns15w, df_sns16w, df_sns17w]
+for site in nue_sites:
+#    print(len(sorted(site['rate_n_applied_kgha'].unique())))
+    print(sorted(site['rate_n_applied_kgha'].unique()))
+for site in sns_sites:
+#    print(len(sorted(site['rate_n_applied_kgha'].unique())))
+    print(sorted(site['rate_n_applied_kgha'].unique()))
+
+# In[Print CVs for each rep]
+cv_list = []
+for rate in sorted(df_nue14sc_pre['rate_n_applied_kgha'].unique()):
+    sd = df_nue14sc_pre[df_nue14sc_pre['rate_n_applied_kgha'] == rate]['yld_grain_dry_kgha'].std()
+    avg = df_nue14sc_pre[df_nue14sc_pre['rate_n_applied_kgha'] == rate]['yld_grain_dry_kgha'].mean()
+    cv_list.append(sd / avg)
+    print(sd / avg)
+print(mean(cv_list))
+
+# In[Print mean yield for subsets]
+yield_list1 = []
+yield_list2 = []
+yield_list3 = []
+yield_list4 = []
+for rep in sorted(df_nue14sc_pre['rep'].unique()):
+    if rep <= 4:
+        yield_list1.append(df_nue14sc_pre[df_nue14sc_pre['rep'] == rep]['yld_grain_dry_kgha'].mean())
+    if rep > 4 and rep <=8:
+        yield_list2.append(df_nue14sc_pre[df_nue14sc_pre['rep'] == rep]['yld_grain_dry_kgha'].mean())
+    if rep > 9 and rep <=12:
+        yield_list3.append(df_nue14sc_pre[df_nue14sc_pre['rep'] == rep]['yld_grain_dry_kgha'].mean())
+    if rep > 13 and rep <=16:
+        yield_list4.append(df_nue14sc_pre[df_nue14sc_pre['rep'] == rep]['yld_grain_dry_kgha'].mean())
+print(mean(yield_list1))
+print(mean(yield_list2))
+print(mean(yield_list3))
+print(mean(yield_list4))
+
+# In[Plot]
+import seaborn as sns
+sns.scatterplot(x='rate_n_applied_kgha', y=y2, data=df_nue14sc_pre)
+
+sns.scatterplot(x='rate_n_applied_kgha', y='yld_grain_dry_kgha', data=df_sns15w_pre)
+
+# In[Add column for grain yield RMSE]
+df_results = my_eonr.df_results
+df_results = df_results.assign(yld_rmse = df_results['grtn_rmse'] / my_eonr.price_grain)
+df_results.to_clipboard()
+
+# In[Split Stewart site]
+def func(df, rep_u=4):
+    yield_list1 = []
+    yield_list2 = []
+    yield_list3 = []
+    yield_list4 = []
+    for rep in sorted(df['rep'].unique()):
+        if rep <= rep_u:
+            yield_list1.append(df[df['rep'] == rep]['yld_grain_dry_kgha'].mean())
+        if rep > rep_u and rep <=6:
+            yield_list2.append(df[df['rep'] == rep]['yld_grain_dry_kgha'].mean())
+#        if rep > 9 and rep <=12:
+#            yield_list3.append(df[df['rep'] == rep]['yld_grain_dry_kgha'].mean())
+        if rep > 6 and rep <=16:
+            yield_list4.append(df[df['rep'] == rep]['yld_grain_dry_kgha'].mean())
+    print('Mean: {0:.2f}   Std: {1:.2f}'.format(np.mean(yield_list1), np.std(yield_list1)))
+    print('Mean: {0:.2f}   Std: {1:.2f}'.format(np.mean(yield_list2), np.std(yield_list2)))
+#    print('Mean: {0:.2f}   Std: {1:.2f}'.format(np.mean(yield_list3), np.std(yield_list3)))
+    print('Mean: {0:.2f}   Std: {1:.2f}'.format(np.mean(yield_list4), np.std(yield_list4)))
+#    print(mean(yield_list2))
+##    print(mean(yield_list3))
+#    print(mean(yield_list4))
+    print('Overall mean: {0}'.format(df['yld_grain_dry_kgha'].mean()))
+
+func(df_nue12s_pre, rep_u=5)
+func(df_nue12s_pre, rep_u=4)
+func(df_nue12s_pre, rep_u=3)
+func(df_nue12s_pre, rep_u=2)
+func(df_nue12s_pre, rep_u=1)